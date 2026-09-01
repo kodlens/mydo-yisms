92f14efeb00fcb20ddb2aee0d8c2bc1f853d0e35
eshen <etiennewayne@gmail.com>
2026-09-01 17:02:54 +0800

    change layout for forgot-password

--- resources/js/pages/auth/forgot-password.tsx
+++ resources/js/pages/auth/forgot-password.tsx
@@ -5,55 +5,64 @@ import { FormEventHandler } from 'react';
 
 import InputError from '@/components/input-error';
 import TextLink from '@/components/text-link';
-import AuthLayout from '@/layouts/auth-layout';
 
 export default function ForgotPassword({ status }: { status?: string }) {
-    const { data, setData, post, processing, errors } = useForm({
-        email: '',
-    });
+  const { data, setData, post, processing, errors } = useForm({
+    email: '',
+  });
 
-    const submit: FormEventHandler = (e) => {
-        e.preventDefault();
+  const submit: FormEventHandler = (e) => {
+    e.preventDefault();
 
-        post(route('password.email'));
-    };
+    post(route('password.email'));
+  };
 
-    return (
-        <AuthLayout title="Forgot password" description="Enter your email to receive a password reset link">
-            <Head title="Forgot password" />
+  return (
+    <>
+     <Head title="Forgot password" />
+      <div className='min-h-screen flex items-center justify-center '>
+        <div className='bg-white p-6 border shadow-lg w-xl rounded-2xl'>
+          {status && <div className="mb-4 text-center text-sm font-medium text-green-600">{status}</div>}
 
-            {status && <div className="mb-4 text-center text-sm font-medium text-green-600">{status}</div>}
+          <div>
+            <img src='/images/e-kabataan.png' height={100} width={100} className='block mx-auto' />
+          </div>
+          <div className='font-bold text-2xl my-4 text-center'>
+            Change Password
+          </div>
+          <div className="space-y-6">
+            <form onSubmit={submit}>
+              <div className="grid gap-2">
+                <label htmlFor="email" className="text-sm font-medium">Email address</label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  autoComplete="off"
+                  value={data.email}
+                  autoFocus
+                  onChange={(e) => setData('email', e.target.value)}
+                  placeholder="email@example.com"
+                />
 
-            <div className="space-y-6">
-                <form onSubmit={submit}>
-                    <div className="grid gap-2">
-                        <label htmlFor="email" className="text-sm font-medium">Email address</label>
-                        <Input
-                            id="email"
-                            type="email"
-                            name="email"
-                            autoComplete="off"
-                            value={data.email}
-                            autoFocus
-                            onChange={(e) => setData('email', e.target.value)}
-                            placeholder="email@example.com"
-                        />
+                <InputError message={errors.email} />
+              </div>
 
-                        <InputError message={errors.email} />
-                    </div>
+              <div className="my-6 flex items-center justify-start">
+                <Button htmlType="submit" type="primary" loading={processing} className="w-full" disabled={processing}>
+                  Email password reset link
+                </Button>
+              </div>
+            </form>
 
-                    <div className="my-6 flex items-center justify-start">
-                        <Button htmlType="submit" type="primary" loading={processing} className="w-full" disabled={processing}>
-                            Email password reset link
-                        </Button>
-                    </div>
-                </form>
-
-                <div className="text-muted-foreground space-x-1 text-center text-sm">
-                    <span>Or, return to</span>
-                    <TextLink href={route('login')}>log in</TextLink>
-                </div>
+            <div className="text-muted-foreground space-x-1 text-center text-sm">
+              <span>Or, return to</span>
+              <TextLink href={route('student-login')}>log in</TextLink>
             </div>
-        </AuthLayout>
-    );
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
 }
